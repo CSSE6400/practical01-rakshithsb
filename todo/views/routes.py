b1e10c8e91aee5ad1e98cd41a630f3c525d2a232
Rakshith Sharanabasappa <rakshith.s.budihal@gmail.com>
2023-02-22 11:37:21 +1000

    "Made changes to completed flag"

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -23,7 +23,7 @@ def get_todo(id):
     "id": id,
     "title": "Watch CSSE6400 Lecture",
     "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-    "completed": False,
+    "completed": True,
     "deadline_at": "2023-02-27T00:00:00",
     "created_at": "2023-02-20T00:00:00",
     "updated_at": "2023-02-20T00:00:00"
